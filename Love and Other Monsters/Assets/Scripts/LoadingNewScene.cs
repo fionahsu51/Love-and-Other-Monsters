a34a83d8b89a9b7d1edc992d66ad5ea4fa8075cf
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-05-01 20:01:00 -0700

    added a loading scene with carriage SFX

---
+++ Love and Other Monsters/Assets/Scripts/LoadingNewScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingNewScene : MonoBehaviour
+{
+    public int SceneIndex;
+    public AudioSource carriageSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carriageSFX.Play();
+        StartCoroutine(AudioBuffer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator AudioBuffer()
+    {
+        yield return new WaitForSeconds(2.2f);
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
