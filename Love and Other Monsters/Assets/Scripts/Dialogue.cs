926b6c0658413afd43623a9ec92bce371b087fd1
Malachi Maldonado <m.maldonado1792@gmail.com>
2023-05-01 22:42:34 -0700

    Added choice handling in dialogue system

--- Love and Other Monsters/Assets/Scripts/Dialogue.cs
+++ Love and Other Monsters/Assets/Scripts/Dialogue.cs
@@ -5,6 +5,7 @@ using TMPro;
 using Ink.Runtime;
 
 //Tutorial from here https://www.youtube.com/watch?v=8oTYabhj248&ab_channel=BMo
+//https://www.youtube.com/watch?v=vY0Sk93YUhA&list=PL3viUl9h9k78KsDxXoAzgQ1yRjhm7p8kl&index=2
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
@@ -12,6 +13,11 @@ public class Dialogue : MonoBehaviour
     
     private Story currentStory;
     private string currentLine;
+
+    //choices
+    public GameObject [] choices;
+
+    private TextMeshProUGUI [] choiceText;
     
     //public string[] lines;
     public float textSpeed;
@@ -22,7 +28,18 @@ public class Dialogue : MonoBehaviour
     void Start()
     {
         textComponent.text = string.Empty;
+
+        //get choices if there are choices
+        choiceText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices)
+        {
+            choiceText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+
         StartDialogue(inkFile);
+        
     }
 
     // Update is called once per frame
@@ -30,7 +47,7 @@ public class Dialogue : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            if(textComponent.text == currentLine)
+            if(currentStory.currentChoices.Count == 0 && textComponent.text == currentLine)
             {
                 continueStory();
             }
@@ -59,6 +76,7 @@ public class Dialogue : MonoBehaviour
             textComponent.text = "";
             currentLine = currentStory.Continue();
             StartCoroutine(TypeLine());
+            DisplayChoices();
         }else{
             endDialogue();
         }
@@ -78,6 +96,32 @@ public class Dialogue : MonoBehaviour
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if(currentChoices.Count > choices.Length){
+            Debug.Log("Too many choices in ink story");
+        }
+
+        int index = 0;
+        foreach(Choice choice in currentChoices){
+            choices[index].gameObject.SetActive(true);
+            choiceText[index].text = choice.text;
+            index++;
+        }
+
+        for(int i = index; i < choices.Length; i++){
+            choices[i].SetActive(false);
+        }
+
+    }
+
+    public void MakeChoice(int choiceIndex){ 
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        continueStory();
     }
 
     /*void NextLine()
--- Love and Other Monsters/Assets/Writing/dialogue_prototype.ink
+++ Love and Other Monsters/Assets/Writing/dialogue_prototype.ink
@@ -1,8 +1,31 @@
-You have seven days to discover what happened to your father
-Each day will start in the morning where the player can review events from previous dates in her journal and will receive letters from the girls’ familiars
-The place will get two choices of where to go, one at noon and one in the evening
-At night the player can write one of the girls a letter and your familiar will deliver it
-The new Van Helsing will be appointed at the end of the seven day midnight during a full moon. 
-During the night of the final day, instead of writing letter, the player must choose the evidence to present in the trial. 
-A perfect run will be triggered if the player chooses the three correct pieces of evidence, each unlocked when they romance each of the girls.
+Who is best girl?
+* Aveline. Vampires are hot.
+    You're so right. Vampires are so hot.
+    But what's your favorite thing about Aveline?
+    ** Her tragic backstory.
+        She needs a hug. 
+    ** Her immaculate fashion sense.
+        I wish I had that dress!
+    ** Her hypnotic eyes... 
+        She doesn't need to hypnotize me to make me do whatever she wants.
+* Dolores. She's so lively!
+    Poor choice of words...
+    How will you let her story play out?
+    ** Let her become a ghost!
+        I'm still wondering how that'll play out.
+        The logistics and everything...
+    ** Turn her into a vampire.
+        Vampires are really hot...
+    ** Save her!!
+        Right?? What's wrong with being human?
+* Freya.......big...woman...
+    unga bunga
+    ** unga
+        unga bunga
+    ** bunga
+        sjdhgkjshfdglkjhdkalJSDH
+    ** I want her to pick me up and ***** my **** until **** and th
+        chill
+
+
 
